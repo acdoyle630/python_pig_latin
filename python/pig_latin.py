45087f6367140e793f6137da7465a2cb323a6513
Adam <acdoyle630@gmail.com>
2017-06-17 18:33:31 -1000

    pig lating python

--- python/pig_latin.py
+++ python/pig_latin.py
@@ -8,11 +8,8 @@ def pigLatin(word):
     elif word_list.index(letters) >= 0:
       first_vowel = word_list.index(letters)
       word_list_vowels.extend([first_vowel])
-  print word_list_vowels
-  print min(word_list_vowels)
   endLetter = word_list[min(word_list_vowels) - 1]
-  print endLetter
   word_list.remove(word_list[min(word_list_vowels) - 1])
   word_list.extend(endLetter + "ay")
-  print "".join(word_list)
-pigLatin('football')
+  return "".join(word_list)
+print pigLatin('football')
