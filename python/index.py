c448e633cfeb4f829fe6554b4f0d5093848f0ce8
Adam <acdoyle630@gmail.com>
2017-06-17 17:37:43 -1000

    palindrome check

--- python/index.py
+++ python/index.py
@@ -1,4 +1,10 @@
-if 5 == 7:
-  print 'five'
-elif 5 > 4:
-  print 'four'
+def pigLatin(string):
+  reverseList = []
+  for letters in reversed(list(string)):
+    reverseList.extend(letters)
+  if string == "".join(reverseList):
+    return True
+  elif string != "".join(reverseList):
+    return False
+
+print pigLatin("bob")
